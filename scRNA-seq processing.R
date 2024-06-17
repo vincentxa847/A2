6fa7d2b0d166d713d9dbbdaa8c2a62107101957c
Ming-Hsuan Lin <vincent23495380@gmail.com>
2024-06-17 11:14:26 +0800

    Try differrent integration and DE method

--- scRNA-seq processing.R
+++ scRNA-seq processing.R
@@ -251,3 +251,64 @@ Idents(Cd40lg) = "orig.ident"
 Cd40lg.KO_affected = FindMarkers(Cd40lg, ident.1 = "KO", ident.2 = "WT",only.pos = TRUE)
 # Two genes, Areg and Gzmb
 Cd40lg.KO_affected_filtered = subset(Cd40lg.KO_affected,avg_log2FC > 0.5 & p_val_adj <0.01) 
+
+#### Using Harmony as integration method ####
+# Merge four Seurat objects into one (Note that some cell names are duplicated across objects, therefore 
+# using add.cell.ids to distinguish cell names from different datasets)
+# The identifiers of two conditions (KO & WT) are stored in meta.data "orig.ident"
+four_samples_combined = merge(WT1.data,WT2.data, add.cell.ids = c("WT1", "WT2"))
+four_samples_combined = merge(four_samples_combined,WT3.data, add.cell.ids = c("WT1_2", "WT3"))
+four_samples_combined = merge(four_samples_combined,KO1.data, add.cell.ids = c("WT", "KO"))
+
+# QC for combined Seurat
+four_samples_combined[["percent.mt"]] = PercentageFeatureSet(four_samples_combined, pattern = "^mt-")
+four_samples_combined = subset(four_samples_combined, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+
+# Normalize, FindVariableFeatures, ScaleData and RunPCA for combined Seurat
+four_samples_combined = NormalizeData(four_samples_combined)
+four_samples_combined = FindVariableFeatures(four_samples_combined,selection.method = "vst", nfeatures = 2000)
+four_samples_combined = ScaleData(four_samples_combined, features = rownames(four_samples_combined))
+four_samples_combined = RunPCA(four_samples_combined, npcs = 30, verbose = FALSE)
+DimPlot(object = four_samples_combined, reduction = "pca",group.by = "orig.ident")
+
+# Run Harmony
+# the result of harmony will be stored in @reduction
+# Note for UMAP and clustering, using "reduction = harmony"
+four_samples_combined.harmony = RunHarmony(four_samples_combined, group.by.vars = "orig.ident", plot_convergence = TRUE)
+DimPlot(object = four_samples_combined.harmony, reduction = "harmony",group.by = "orig.ident")
+four_samples_combined.harmony = RunUMAP(four_samples_combined.harmony, reduction = "harmony",dims = 1:30)
+# Downstream analysis
+ElbowPlot(four_samples_combined.harmony)
+four_samples_combined.harmony = FindNeighbors(four_samples_combined.harmony, reduction = "harmony", dims = 1:14)
+four_samples_combined.harmony = FindClusters(four_samples_combined.harmony, resolution = 0.5)
+DimPlot(four_samples_combined.harmony, reduction = "umap", label = TRUE)
+DimPlot(four_samples_combined.harmony, reduction = "umap", group.by = "orig.ident", label = TRUE)
+
+# Filter away noise, and DE analysis on cluster 5 (largest cluster that contains cells from the KO and the WT)
+four_samples_combined.harmony.filtered = subset(four_samples_combined.harmony,idents = c(7,8,9,10,11),invert = TRUE)
+
+cluster.5.harmony = subset(four_samples_combined.harmony.filtered, idents = "5")
+Idents(cluster.5.harmony) = "orig.ident"
+cluster.5.harmony.KO_affected = FindMarkers(cluster.5.harmony, ident.1 = "KO", ident.2 = "WT", verbose = FALSE,only.pos = TRUE)
+# 67 genes, top 5 are "Itgb1","AC163354.1","Gzmb" ,"Klrk1","Tagln2" 
+cluster.5.harmony.KO_affected_filtered = subset(cluster.5.harmony.KO_affected,avg_log2FC > 0.5 & p_val_adj <0.01)
+row.names(cluster.5.harmony.KO_affected_filtered)
+
+#### Using MAST as DE method ####
+# For default integration method
+Cd40lg.MAST.KO_affected = FindMarkers(Cd40lg, ident.1 = "KO", ident.2 = "WT", test.use = "MAST", only.pos = TRUE)
+Cd40lg.MAST.KO_affected_filtered = subset(Cd40lg.MAST.KO_affected,avg_log2FC > 0.5 & p_val_adj <0.01)
+row.names(Cd40lg.MAST.KO_affected_filtered) # 2 GENES , "Gzmb" "Areg"
+
+# For Harmony intergation method
+cluster.5.MAST.harmony.KO_affected = FindMarkers(cluster.5.harmony, ident.1 = "KO", ident.2 = "WT", test.use = "MAST",only.pos = TRUE)
+cluster.5.MAST.harmony.KO_affected_filtered = subset(cluster.5.MAST.harmony.KO_affected,avg_log2FC > 0.5 & p_val_adj <0.01)
+# 67 genes, top 5 are "Itgb1" "Gzmb" "Cd163l1" "AC163354.1" "Klrk1"
+row.names(cluster.5.MAST.harmony.KO_affected)
+
+#### Save the result of DE analysis with four different methods ####
+write.table(Cd40lg.KO_affected_filtered, file='default_integration_and_DEmethod.tsv', quote=FALSE, sep='\t',col.names = NA)
+write.table(cluster.5.harmony.KO_affected_filtered, file='Harmony_integration_and_Default_DEmethod.tsv', quote=FALSE, sep='\t')
+write.table(Cd40lg.MAST.KO_affected_filtered, file='default_integration_and_MAST_DEmethod.tsv', quote=FALSE, sep='\t',col.names = NA)
+write.table(cluster.5.MAST.harmony.KO_affected, file='Harmony_integration_and_MAST_DEmethod.tsv', quote=FALSE, sep='\t',col.names = NA)
+
